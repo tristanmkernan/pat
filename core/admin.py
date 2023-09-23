8c9d2a038ba572fc6d337bfec43a8680d807f131
Tristan Kernan <tristanmkernan@gmail.com>
2023-09-23 11:22:24 -0400

    feat: add compliment admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Accomplishment
+from .models import Accomplishment, Compliment
 
 
 class AccomplishmentAdmin(admin.ModelAdmin):
@@ -14,4 +14,16 @@ class AccomplishmentAdmin(admin.ModelAdmin):
     list_select_related = ("owner",)
 
 
+class ComplimentAdmin(admin.ModelAdmin):
+    list_display = (
+        "uuid",
+        "owner",
+        "source",
+        "created_at",
+    )
+
+    list_select_related = ("owner",)
+
+
 admin.site.register(Accomplishment, AccomplishmentAdmin)
+admin.site.register(Compliment, ComplimentAdmin)
